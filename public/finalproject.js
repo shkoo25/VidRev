4c899f359fd1c4151861af3d6fd63cc4e46566bb
Steve Koo <shkoo25@gmail.com>
2015-07-27 11:29:34 -0400

    Final Project

--- public/finalproject.js
+++ public/finalproject.js
@@ -1,64 +1,55 @@
 var app = angular.module("reviewApp", ["ngRoute"])
 
- 
-    app.controller("baseController", function($scope, $http, $sce){
-            $http.get("https://vidrev.herokuapp.com")
-             .success(function(data){
-                 $scope.reviews = data.Metacritic
-                 var youtubeData = data.Youtube
-              youtubeData = youtubeData.map(function(video){
-                video.videoUrl = $sce.trustAsResourceUrl("https://www.youtube.com/embed/" + video.videoId)
-                return video
-              })
-                 $scope.youtubes = youtubeData
-                 var twitchData = data.Twitch
-                 console.log('Here1')
-                 $scope.clickedOn = {}
 
-                $scope.clickPlay = function(twitch) {
-                  $scope.clickedOn[twitch.stream_id] = true
-                  
-                }
-
-                twitchData = twitchData.map(function(video){
-                    console.log('Here1.5')
-                    $scope.clickedOn[video.stream_id] = false
-                    console.log('Here2')
-                   video.videoUrl = $sce.trustAsResourceUrl(video.links + "/embed")
-                   console.log('Here3')
-                    return video
-
-                })
-                console.log('Here4')
-                $scope.twitches = data.Twitch 
-
-
-             })
-            
+    app.controller("searchController", function($scope){
 
     })
+ 
+    app.controller("baseController", function($scope, $http, $sce, $routeParams){
+      var game = $routeParams.game_name
+      
+      $http.get("https://vidrev.herokuapp.com/?search=" + game).success(function(data){
+        
+        console.log("data", data)
 
-           
+
+          $scope.reviews = data.Metacritic
+          var youtubeData = data.Youtube
+          youtubeData = youtubeData.map(function(video){
+            video.videoUrl = $sce.trustAsResourceUrl("https://www.youtube.com/embed/" + video.videoId)
+            return video
+          })
+          $scope.youtubes = youtubeData
 
 
-    app.controller("searchController", function($scope, $http, $location){
-      $scope.submit = function() {
-              if ($scope.text){
-                 
-                 $http.get("http://389b0914.ngrok.io/?search=" + $scope.text)
-                      .success(function(data){
-                        //$location.url('/results')
-                      
-                      })
-                      $scope.text = ''
+          var twitchData = data.Twitch
+          $scope.clickedOn = {}
+
+          $scope.clickPlay = function(twitch) {
+            $scope.clickedOn[twitch.stream_id] = true
           }
-      }
-  })
+
+          twitchData = twitchData.map(function(video){
+            $scope.clickedOn[video.stream_id] = false
+            video.videoUrl = $sce.trustAsResourceUrl(video.links + "/embed")
+            return video
+
+          })
+    
+          $scope.twitches = data.Twitch 
+     
+          $scope.text = ''
+        //}
+        
+                     
+      })
+    })
+
   app.config(function($routeProvider){
 
     $routeProvider
       
-      .when("/results",{
+      .when("/results/:game_name",{
         templateUrl: "templates/results.html"
       })
       .otherwise({
