5fbdd3f0af9a803b6063d49b76cc4de62f7a7a6d
Steve Koo <shkoo25@gmail.com>
2015-07-29 09:46:08 -0400

    Final Project

--- public/finalproject.js
+++ public/finalproject.js
@@ -2,13 +2,53 @@ var app = angular.module("reviewApp", ["ngRoute"])
 
 
     app.controller("searchController", function($scope){
-      
+      (function() {
+
+  // cache vars
+  var cards = document.querySelectorAll(".card.effect__random");
+  var timeMin = 3;
+  var timeMax = 15;
+  var timeouts = [];
+
+  // loop through cards
+  for ( var i = 0, len = cards.length; i < len; i++ ) {
+    var card = cards[i];
+    var cardID = card.getAttribute("data-id");
+    var id = "timeoutID" + cardID;
+    var time = randomNum( timeMin, timeMax ) * 1000;
+    cardsTimeout( id, time, card );
+  }
+
+  // timeout listener
+  function cardsTimeout( id, time, card ) {
+    if (id in timeouts) {
+      clearTimeout(timeouts[id]);
+    }
+    timeouts[id] = setTimeout( function() {
+      var c = card.classList;
+      var newTime = randomNum( timeMin, timeMax ) * 1000;
+      c.contains("flipped") === true ? c.remove("flipped") : c.add("flipped");
+      cardsTimeout( id, newTime, card );
+    }, time );
+  }
+
+  // random number generator given min and max
+  function randomNum( min, max ) {
+    return Math.random() * (max - min) + min;
+  }
+
+})();
+
         $scope.submit = function(){
           window.location.href = "#results/" + $scope.text
           $scope.text = ''
+
         }
 
+
      })
+
+
 
  
     app.controller("baseController", function($scope, $http, $sce, $routeParams){
